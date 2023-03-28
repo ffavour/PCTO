a54d89ae4ccfa26ed0b89392261334ecc359d172
aandrix <113350273+aandrix@users.noreply.github.com>
2023-03-28 08:27:53 +0200

    walla

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PCTO
 progetto PCTO
+
+
+ok per ora ho messo la roba di p5 i style sheet in css e giusto un immagine per vedere se funzionava, mi garbava come sfondo del gioco 
---
+++ prova1/codeJS/home.js
@@ -0,0 +1,28 @@
+// Setup code 
+function preload(){
+    sfondo = loadImage("images/sfondoSchermataPrincipale.png");
+
+}
+
+function setup () {
+
+    createCanvas(displayWidth, displayHeight);
+
+}
+
+
+
+
+
+function draw () {    
+    
+  
+    background(sfondo);
+
+
+
+
+}
+
+
+
