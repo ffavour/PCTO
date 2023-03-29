76d4e8c346db03f048cba28db374b92cddb79239
aandrix <113350273+aandrix@users.noreply.github.com>
2023-03-29 08:29:42 +0200

    sei felice ho tolto l'obrina

--- prova1/codeJS/home.js
+++ prova1/codeJS/home.js
@@ -97,7 +97,7 @@ function inizializzaBottoni(){
     bottoneStart = new Bottone((width/2 - (200/2)), height-200, bottoneStartImg, 200,80);
     bottoneHome = new Bottone(width - 90, height - 85, bottoneHomeImg, 75, 75);
     bottoneScorreSX = new Bottone((width/2 - (75/2) - 300), (height/2 - (75/2)), scorreSxImg, 75, 80);
-    bottoneScorreDX = new Bottone((width/2 - (75/2) + 300), (height/2 - (75/2)), scorreDxImg, 75, 75);
+    bottoneScorreDX = new Bottone((width/2 - (75/2) + 300), (height/2 - (75/2)), scorreDxImg, 75, 80);
 }
 
 
