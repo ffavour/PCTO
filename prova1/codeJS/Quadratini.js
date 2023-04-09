3013a857e1b7c6fe29f7a94a0d4e593ca6baec9d
ffavour <favour.osasere@itiscuneo.eu>
2023-04-09 12:53:45 +0200

    Update Quadratini.js

--- prova1/codeJS/Quadratini.js
+++ prova1/codeJS/Quadratini.js
@@ -1,11 +1,11 @@
-class Quadratini{
+class Quadratini {
 
    static xSpeed = 45;
    static width1 = 150;
    static height1 = 50;
 
    static imageLibero = quadratinoImg;
-   static posXinit = width-50;
+   static posXinit = width - 50;
    static limX = 600; //era 500 ma è meglio 600
 
    constructor(posY) {
@@ -14,43 +14,30 @@ class Quadratini{
 
    }
 
-   drawOso(){
+   drawOso() {
       image(quadratinoImg, this.posX, this.posY, Quadratini.width1, Quadratini.height1);
    }
 
-   move(){
+   move() {
       //delay(500);
-      if(this.posX - Quadratini.xSpeed < Quadratini.limX){
+      if (this.posX - Quadratini.xSpeed < Quadratini.limX) {
          return false;
-      }else{
+      } else {
          this.posX = this.posX - Quadratini.xSpeed;
          return true;
       }
    }
 
    //ma serve???
-   moveAndDraw(){
-      if(this.move()){
+   moveAndDraw() {
+      if (this.move()) {
          this.drawOso();
          return true;
-      }else{
+      } else {
          this.drawOso();
          return false;
       }
    }
 
-   //da guardare
-   cursoreToccato(){
-      /*if(mouseX > this.posX && mouseX < this.posX + this.widthR &&
-       mouseY > this.posY && mouseY < this.posY + this.heightR){
-      this.colore = 'green';
-    }*/
-      var curX, cursY, selez = game.drawKeypointsGioco();
-
-      if(mouseX > this.posY && mouseX < this.posX + Quadratini.width1 &&
-         mouseY > this.posY && mouseY < this.posY + Quadratini.height1){
-         Quadratini.imageLibero = quadratinoImgPremutoImg;
-      }
-   }
-
 }
+
